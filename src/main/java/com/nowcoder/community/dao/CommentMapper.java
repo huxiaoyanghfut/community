1a65001b93a1db841d716a92293e97b5a4999490
timeshell <huxiaoyanghfut@gmail.com>
2021-09-21 19:02:24 +0800

    显示评论

---
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -0,0 +1,21 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author:xiaoyang
+ * @Title: CommentMapper
+ * @ProjectName: community
+ * @Description: TODO
+ * @date: 2021/09/21 16:21
+ */
+@Mapper
+public interface CommentMapper {
+    //根据实体(帖子)，实体类型：评论和回复，来查询一页评论数据
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+    //统计所有评论数据
+    int selectCountByEntity(int entityType, int entityId);
+}
